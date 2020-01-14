5a980a590c15796e8ea82568c5c6273e9c84fd44
jose <jmvr37@hotmail.com>
2020-01-13 19:49:07 -0800

    first commit

---
+++ App.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={[styles.box, styles.box1]} />
+        <View style={[styles.box, styles.box2]} />
+        <View style={[styles.box, styles.box3]} />
+        <View style={[styles.box, styles.box4]} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    backgroundColor: '#F5FCFF',
+  },
+  box: {
+    width: '50%',
+    height: '50%',
+  },
+  box1: {
+    backgroundColor: 'powderblue',
+  },
+  box2: {
+    backgroundColor: 'skyblue',
+  },
+  box3: {
+    backgroundColor: 'steelblue',
+  },
+  box4: {
+    backgroundColor: 'midnightblue',
+  },
+});
+
+export default App;
